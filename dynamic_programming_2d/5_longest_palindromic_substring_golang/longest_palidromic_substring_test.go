9c3ca101a9defa60a81a5e4144928bd98c29e839
gary.y <s14031403@gmail.com>
2024-04-10 21:18:49 +0800

    add test case for go solution

--- dynamic_programming_2d/5_longest_palindromic_substring_golang/longest_palidromic_substring_test.go
+++ dynamic_programming_2d/5_longest_palindromic_substring_golang/longest_palidromic_substring_test.go
@@ -1,1 +1,11 @@
 package palindromic_substring
+
+import "testing"
+
+// Test case 1
+func TestLongestPalindromicSubstring1(t *testing.T) {
+	result := longestPalindrome("babad")
+	if result != "aba" && result != "bab" {
+		t.Errorf("Got: %s, want: aba or bab", result)
+	}
+}
